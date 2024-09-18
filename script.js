e2c244baec79621167eb3ee5e10675e979760c8f
khasarah <jf0844.khasarah@gmail.com>
2024-09-18 14:14:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,23 @@ function hide() {
 }
 
 async function send() {
-	let file = document.getElementById('file').files[0] ? 1 : 0;
-	await alert(["Наипать решил? фотку кидай", "Фотка принята, давай ищо"][file]);
-	if (file) {
-		location.reload()
+	let file = document.getElementById('file').files[0];
+	let answ;
+	file ? answ = 1 :  answ = 0;
+	if (answ == 0) {
+		await alert("Ты каво наипать решил, новеньки? Скинь фотку.");
+		return;
 	}
+
+	file.type.split('/')[0] == "image" ? answ = 1 : answ = -1;
+
+	if (answ == -1) {
+		await alert("Я же просил фотку, а не документи. Я жду от тебя чесности, новеньки.");
+		return;
+	}
+	
+	await alert("Моладец, новеньки, ты мне нравишся. Скинь еще фотку.")
+	location.reload()
 }
 
+// https://khasarah.github.io/caterpillar/
